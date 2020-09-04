1d2af7c824fa48d790d64295201980cfc16c24b3
bokuweb <bokuweb12@gmail.com>
2020-09-05 00:48:58 +0900

    feat: Add mov1 (#7)

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -162,6 +162,9 @@ impl ARM {
                 thumb::Instruction::LSL(dec) => {
                     exec_thumb_lsl(bus, dec, &mut self.gpr, &mut self.cpsr)?
                 }
+                thumb::Instruction::MOV1(dec) => {
+                    exec_thumb_mov1(dec, &mut self.gpr, &mut self.cpsr)?
+                }
                 thumb::Instruction::B(dec) => exec_thumb_b(dec, &mut self.gpr, &mut self.cpsr)?,
                 _ => unimplemented!(),
             }
--- src/cpu/decoder/thumb/data_processing.rs
+++ src/cpu/decoder/thumb/data_processing.rs
@@ -2,10 +2,12 @@ bitfield! {
     #[derive(Debug, PartialEq, Clone, Copy)]
     pub struct DataProcessing(u16);
     pub get_op, _: 12, 11;
+    #[allow(non_snake_case)]
+    pub get_Rd10_8, _: 10, 8;
     pub get_sh, _: 10, 6;
     #[allow(non_snake_case)]
     pub get_Rn, _: 5, 3;
+    pub get_imm8, _: 7, 0;
     #[allow(non_snake_case)]
-    pub get_Rd, _: 3, 0;
+    pub get_Rd2_0, _: 2, 0;
 }
-
