69a783489efcabdf3c939bbde1816877c9357af2
bokuweb <bokuweb12@gmail.com>
2020-09-05 01:36:10 +0900

    feat: Add thumb bl (#8)

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -166,6 +166,7 @@ impl ARM {
                     exec_thumb_mov1(dec, &mut self.gpr, &mut self.cpsr)?
                 }
                 thumb::Instruction::B(dec) => exec_thumb_b(dec, &mut self.gpr, &mut self.cpsr)?,
+                thumb::Instruction::BL(dec) => exec_thumb_bl(dec, &mut self.gpr)?,
                 _ => unimplemented!(),
             }
         };
