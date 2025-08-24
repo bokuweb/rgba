c8c80bcf7b84b9ddb59390eba6f635cc0fa93b74
bokuweb <bokuweb12@gmail.com>
2025-08-24 14:33:54 +0900

    dma

--- src/gba/mod.rs
+++ src/gba/mod.rs
@@ -1,4 +1,5 @@
 mod bus;
+mod dma;
 
 use crate::io;
 use crate::lcd;
@@ -83,6 +84,10 @@ impl GBA {
     pub fn frame(&mut self, started: bool) -> Vec<u8> {
         loop {
             let cycles = self.arm.step(&mut self.bus, started).unwrap();
+            
+            // Execute any pending DMA transfers
+            self.bus.execute_dma_transfers();
+            
             let lcdc = self.bus.borrow_mut_lcdc();
             let ready = lcdc.run(cycles);
             if ready {
