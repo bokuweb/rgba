5e75fb675e4d1cdd09151388e529229085c21a4b
bokuweb <bokuweb12@gmail.com>
2025-09-28 02:19:18 +0900

    fix 211

--- src/cpu/instructions/thumb/mod.rs
+++ src/cpu/instructions/thumb/mod.rs
@@ -3,9 +3,11 @@ mod branch;
 mod branch_and_exchange;
 mod data_processing;
 mod single_data_transfer;
+mod swi;
 
 pub use block_data_transfer::*;
 pub use branch::*;
 pub use branch_and_exchange::*;
 pub use data_processing::*;
 pub use single_data_transfer::*;
+pub use swi::*;
